13a015d901befefc0eef9c2f8837c748abde4a78
rgrace9 <rgracewilliams@gmail.com>
2019-01-23 17:31:34 -0500

    add genres migration, model, tests, controller, index.erb, updated routes.rb, updated seeds.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'podcasts#index'
   resources :podcasts, only: :index
+  resources :genres, only: :index
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_23_151526) do
+ActiveRecord::Schema.define(version: 2019_01_23_210031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "podcasts", force: :cascade do |t|
     t.string "name", null: false
