2740b1162b067ddb864fed996477437a9e48952c
rgrace9 <rgracewilliams@gmail.com>
2019-01-23 09:41:28 -0500

    working code for user authorization

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe User, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+feature 'determine if user is an admin or not an admin', %Q{
+  An admin is a different role than a member} do
+
+
+    pending  "#admin?" do
+      it "is not an admin if the role is not admin" do
+        user = FactoryBot.create(:user, role: "member")
+        expect(user.admin?).to eq(false)
+      end
+
+      it "is an admin if the role is admin" do
+        user = FactoryBot.create(:user, role: "admin")
+        expect(user.admin?).to eq(true)
+      end
+    end
+  end
