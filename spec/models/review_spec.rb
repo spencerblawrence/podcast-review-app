d31ffa1b442205d11dae8ef64a7af2c69ba2e34e
Spencer Lawrence <spencerblawrence@gmail.com>
2019-01-25 10:02:14 -0500

    fix review_spec.rb test

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -8,5 +8,5 @@ describe Review, type: :model do
   it { should_not have_valid(:rating).when(nil, "") }
   it { should_not have_valid(:rating).when(0) }
   it { should_not have_valid(:rating).when(6) }
-  it { should validate_length_of(:body).is_at_least(5).is_at_most(500) }
+  it { should validate_length_of(:body).is_at_least(5).is_at_most(2000) }
 end
