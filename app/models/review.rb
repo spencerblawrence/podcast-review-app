ec6fb2625f229e2e51135b27315d1427a8ff3ef0
Spencer Lawrence <spencerblawrence@gmail.com>
2019-01-25 09:55:42 -0500

    made suggested reviews changes

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :podcast
+  belongs_to :user
 
   validates :rating, presence: true, inclusion: { in: 1..5 }
-  validates :body, presence: true, length: { maximum: 500, minimum: 5 }
+  validates :body, presence: true, length: { maximum: 2000, minimum: 5 }
 end
